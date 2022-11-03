b7537a6dd24bda93c8e3241fe6c983391506dd96
Cdereyes <111031931+Cdereyes@users.noreply.github.com>
2022-11-02 21:07:40 -0500

    Revert "Development"

--- back/routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { registroUsuario } = require("../controllers/authController");
-const router = express.Router();
-
-router.route("/usuario/registro").post(registroUsuario);
-
-module.exports = router;
