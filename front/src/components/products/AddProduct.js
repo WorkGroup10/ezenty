9ee2d279fa6c69808e8e1afc020c67020d1b0d71
Cdereyes <cristiandelosreyesrua@gmail.com>
2022-11-08 18:31:51 -0500

    Add
    Add product

--- front/src/components/products/AddProduct.js
+++ front/src/components/products/AddProduct.js
@@ -1,110 +1,70 @@
-import React, { Fragment } from 'react'
-import MetaData from '../layout/Metadata'
-
-
+import React, { Fragment } from "react";
+import MetaData from "../layout/Metadata";
 
 const AddProduct = () => {
-
-
-    return (
+  return (
+    <Fragment>
+      <MetaData title="Esto es una prueba"></MetaData>
+      <div className="cajadiv">
+        <div className="col-12 col-md-2"></div>
         <Fragment>
-            <MetaData title="Esto es una prueba"></MetaData>
-            <div className="row">
-                <div className="col-12 col-md-2">
-                    
-                </div>
+          <div className="h1">
+            <h1>Nuevo Producto</h1>
+          </div>
 
-                <div className="col-12 col-md-10">
-                    <Fragment>
-                        <div className="wrapper my-5">
-                            <form className="shadow-lg"  encType='multipart/form-data'>
-                                <h1 className="mb-4">Nuevo Producto</h1>
-
-                                <div className="form-group">
-                                    <label htmlFor="name_field">Nombre</label>
-                                    <input
-                                        type="text"
-                                        id="name_field"
-                                        className="form-control"
-                                
-                                    />
-                                </div>
-
-                                <div className="form-group">
-                                    <label htmlFor="price_field">Price</label>
-                                    <input
-                                        type="text"
-                                        id="price_field"
-                                        className="form-control"
-                                        
-                                    />
-                                </div>
-
-                                <div className="form-group">
-                                    <label htmlFor="description_field">Descripcion</label>
-                                    <textarea className="form-control" id="description_field" rows="8"></textarea>
-                                </div>
-
-                                <div className="form-group">
-                                    <label htmlFor="category_field">Categoria</label>
-                                    <select className="form-control" id="category_field">
-
-                                    </select>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="stock_field">Inventario</label>
-                                    <input
-                                        type="number"
-                                        id="stock_field"
-                                        className="form-control"
-                                    />
-                                </div>
-
-                                <div className="form-group">
-                                    <label htmlFor="seller_field">Vendedor</label>
-                                    <input
-                                        type="text"
-                                        id="seller_field"
-                                        className="form-control"
-                                    
-                                    />
-                                </div>
-
-                                <div className='form-group'>
-                                    <label>Imagenes</label>
-
-                                    <div className='custom-file'>
-                                        <input
-                                            type='file'
-                                            name='product_images'
-                                            className='custom-file-input'
-                                            id='customFile'
-                                            multiple
-                                        />
-                                        <label className='custom-file-label' htmlFor='customFile'>
-                                            Seleccione Imagen
-                                     </label>
-                                    </div>
-
-                                </div>
-
-
-                                <button
-                                    id="login_button"
-                                    type="submit"
-                                    className="btn btn-block py-3"
-                                >
-                                    CREAR
-                                </button>
-
-                            </form>
-                        </div>
-                    </Fragment>
-                </div>
+          <table>
+            <div>
+              <label htmlFor="name_field">Nombre</label>
+              <input type="text" id="name_field" className="form-control" />
             </div>
 
-        </Fragment>
-    )
-}
+            <div>
+              <label htmlFor="price_field">Precio</label>
+              <input type="text" id="price_field" className="form-control" />
+            </div>
 
-export default AddProduct
+            <div className="form-group">
+              <label htmlFor="description_field">Descripción</label>
+              <textarea
+                className="form-control"
+                id="description_field"
+                rows="8"
+              ></textarea>
+            </div>
+            <div className="form-group">
+              <label htmlFor="stock_field">Inventario</label>
+              <input type="number" id="stock_field" className="form-control" />
+            </div>
+            <Fragment>
+              <div className="div2">
+                <label>Imagenes</label>
+
+                <div className="custom-file">
+                  <input
+                    type="file"
+                    name="product_images"
+                    className="custom-file-input"
+                    id="customFile"
+                    multiple
+                  />
+                  <label className="custom-file-label" htmlFor="customFile">
+                    Seleccione Imagen
+                  </label>
+                </div>
+              </div>
+            </Fragment>
+            <button
+              id="login_button"
+              type="submit"
+              className="btn btn-block py-3"
+            >
+              CREAR
+            </button>
+          </table>
+        </Fragment>
+      </div>
+    </Fragment>
+  );
+};
+
+export default AddProduct;
